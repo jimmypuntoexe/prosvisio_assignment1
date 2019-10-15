d2de1d9d269ba17fb966a86bb797e620eb2f97f2
alessandro_guidi <a.guidi@campus.unimib.it>
2019-10-15 17:36:01 +0200

    create database

--- Project.py
+++ Project.py
@@ -3,6 +3,8 @@
 import mysql.connector
 from mysql.connector import Error
 import sqlite3
+import datetime
+
 mydb = mysql.connector
 try:
     mydb = mysql.connector.connect(
@@ -13,12 +15,13 @@ try:
     if mydb.is_connected():
         mycursor = mydb.cursor()
         #fare controllo se esiste già il database
-        mycursor.execute("CREATE DATABASE IF NOT EXIST provauno")
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS provauno")
+        mycursor.execute("USE provauno")
         #da mettere in un altra funzione? #inserire foreing key e controllare come creare chiavi composte
-        mycursor.execute("CREATE TABLE IF NOT EXIST Cinema (idCinema INT AUTO_INCREMENT PRIMARY KEY, Nome VARCHAR(50),Città VARCHAR(50), numsale INT)")
-        mycursor.execute("DROP TABLE IF EXIST Film (idFilm INT AUTO_INCREMENT PRIMARY KEY, Titolo VARCHAR(50),Regista VARCHAR(50), durata INT, genere VARCHAR(50), anno YEAR)")
-        mycursor.execute("DROP TABLE IF EXIST Clienti (CF VARCHAR(45) PRIMARY KEY, Cognome VARCHAR(50),Nome VARCHAR(50), Età INT)")
-        mycursor.execute("DROP TABLE IF EXIST Biglietto (Posto INT, Fila VARCHAR(1),sala INT, data DATETIME)")
+        mycursor.execute("CREATE TABLE IF NOT EXISTS Cinema (idCinema INT AUTO_INCREMENT PRIMARY KEY, Nome VARCHAR(50),Città VARCHAR(50), numsale INT)")
+        mycursor.execute("CREATE TABLE IF NOT EXISTS Film (idFilm INT AUTO_INCREMENT PRIMARY KEY, Titolo VARCHAR(50),Regista VARCHAR(50), durata INT, genere VARCHAR(50), anno YEAR)")
+        mycursor.execute("CREATE TABLE IF NOT EXISTS Clienti (CF VARCHAR(45) PRIMARY KEY, Cognome VARCHAR(50),Nome VARCHAR(50), Età INT)")
+        mycursor.execute("CREATE TABLE IF NOT EXISTS Biglietto (Posto INT, Fila VARCHAR(1),sala INT, data DATETIME PRIMARY KEY, FOREIGN KEY(idCinema) REFERENCES Cinema(idCinema), FOREIGN KEY(idFilm) REFERENCES Cinema(idFilm),FOREIGN KEY(CF) REFERENCES Cinema(CF))")
 except Error as e:
     print("Error while connecting to MySQL", e)
 finally:
