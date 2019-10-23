adb73c12fd947f52f9ea1daf209b04a2cb8759d9
alessandro_guidi <a.guidi@campus.unimib.it>
2019-10-23 10:22:36 +0200

    change check_nome/cognome and add service for unit-test

--- check_functions.py
+++ check_functions.py
@@ -29,9 +29,8 @@ def check_nome(nome):
     if not nome:
         return False
     for char in nome:
-        if ((char < 'a' or char > 'z') \
-            and (char < 'A' or char > 'Z') \
-            and char == ' '):
+        if (not((char >= 'a' and char <= 'z') \
+            or (char >= 'A' and char <= 'Z'))):
             return False
 
     return True
@@ -42,9 +41,8 @@ def check_cognome(cognome):
         return False
 
     for char in cognome:
-        if ((char < 'a' or char > 'z') \
-            and (char < 'A' or char > 'Z') \
-            and char == ' '):
+        if (not((char >= 'a' and char <= 'z') \
+            or (char >= 'A' and char <= 'Z'))):
             return False
 
     return True
