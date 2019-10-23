f4fe2f85b923818e6fdecc0208f04237c90a6113
lapuile <g.balducci1@campus.unimib.it>
2019-10-23 18:14:31 +0200

    update test.py

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ import check_functions
 
 class TestCheck (unittest.TestCase):
 
-    def test_check_id(self):
+    #def test_check_id(self):
 
-        self.assertTrue(check_functions.check_user('CF00000000000001'),msg=None)
-        self.assertTrue(check_functions.check_user('CF00000000000002'),msg=None)
-        self.assertFalse(check_functions.check_user('89ER4'),msg=None)
-        self.assertFalse(check_functions.check_user('876'),msg=None)
+       # self.assertTrue(check_functions.check_user('CF00000000000001'))
+       # self.assertTrue(check_functions.check_user('CF00000000000002'))
+       # self.assertFalse(check_functions.check_user('89ER4'))
+       # self.assertFalse(check_functions.check_user('876'))
 
     def test_check_nome(self):
 
