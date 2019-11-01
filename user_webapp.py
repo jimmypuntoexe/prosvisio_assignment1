71b74a79a4de19c31ca1bdbb920113d26b7c0065
lapuile <g.balducci1@campus.unimib.it>
2019-11-01 16:18:52 +0100

    create web app for heroku

---
+++ user_webapp.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import os
+import back_database
+import sqlite3
+
+back_database.create_db()
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    database = sqlite3.connect('ticketapp.db')
+    cursor = database.cursor()
+    
+    cinema = back_database.select_cinema()
+    film = back_database.select_film()
+    biglietto = back_database.print_biglietto()
+
+    database.close()
+
+    return render_template(
+        "index.html", main=True, car = car, rental = rental, client = client
+    )
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
