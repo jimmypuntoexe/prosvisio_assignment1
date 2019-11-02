9af2ea15eeaffee1a7284760538ee3bca0bcfc89
lapuile <g.balducci1@campus.unimib.it>
2019-11-02 19:17:32 +0100

    fix bugs

--- back_database.py
+++ back_database.py
@@ -119,7 +119,7 @@ def found_table(connection):
 def insert_clienti(cf, nome, cognome, age):
 	conn= sqlite3.connect('ticketapp.db')
 	try:
-		conn.execute("INSERT INTO Cliente (cf, nome, cognome) VALUES (?, ?, ?, ?);" %(cf, nome, cognome, age))
+		conn.execute("INSERT INTO Cliente (CF, Nome, Cognome, Età) VALUES (?, ?, ?, ?);" %(cf, nome, cognome, age))
 		conn.commit()
 	except:
 		conn.close()
