fb6b3ef88be97d02a95f61cac12820605067d198
Alessandro Guidi <a.guidi@campus.unimib.it>
2019-11-03 20:41:37 +0000

    Update user_webapp.py

--- user_webapp.py
+++ user_webapp.py
@@ -1,16 +1,17 @@
 '''webapp'''
-from flask import Flask, render_template, request
 import os
-import back_database
 import sqlite3
+from flask import render_template, Flask, request
+import back_database
 
 back_database.create_db()
 APP = Flask(__name__)
 _CF = ""
 ID_CINEMA = ""
 ID_FILM = ""
-'''return home page'''
+
 @APP.route("/")
+'''return home page'''
 def index():
     connection = sqlite3.connect('ticketapp.db')
     cliente, cinema, film = back_database.found_table(connection)
@@ -18,8 +19,9 @@ def index():
     return render_template(
         "index.html", main=True, cliente=cliente, cinema=cinema, film=film
     )
-'''Add client for buy ticket'''
+
 @APP.route('/getreginfouser', methods=['GET', 'POST'])
+'''Add client for buy ticket'''
 def getreginfouser():
     codf = request.form['codice_fiscale']
     nome = request.form['nome']
@@ -41,8 +43,8 @@ def getreginfouser():
         film=film
     )
 
-'''Insert ticket and print it '''
 @APP.route('/printticket', methods=['GET', 'POST'])
+'''Insert ticket and print it '''
 def printticket():
     codf = request.form['codice_fiscale']
     id_c = request.form['cinema_id']
@@ -58,47 +60,6 @@ def printticket():
         film=film
     )
 
-'''
-@app.route('/getFilm', methods=['GET', 'POST'])
-def getFilm():
-    id = request.form['film_id']
-    connection = sqlite3.connect('ticketapp.db')
-    cliente, cinema, film = back_database.found_table(connection)
-    global id_film
-    id_film = id
-    
-
-    return render_template(
-        "index.html", main=True,
-        cliente = cliente,
-        cinema = cinema,
-        film = film
-    )
-    connection.close()
-
-@app.route('/getCinema', methods=['GET', 'POST'])
-def getCinema():
-    
-    id = request.form['cinema_id']
-    connection = sqlite3.connect('ticketapp.db')
-    cliente, cinema, film = back_database.found_table(connection)
-    global id_cinema
-    id_cinema = id
-
-    
-
-    return render_template(
-        "index.html", main=True,
-        cliente = cliente,
-        cinema = cinema,
-        film = film
-    )
-    connection.close()
-'''
-
-
-
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    PORT = int(os.environ.get("PORT", 5000))
+    APP.run(host='0.0.0.0', port=PORT)
